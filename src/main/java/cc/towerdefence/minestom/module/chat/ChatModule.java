7e161dfd164e0ba5dc218c37aa82cd548c0156c3
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2022-11-10 22:01:01 +0000

    Make public

--- src/main/java/cc/towerdefence/minestom/module/chat/ChatModule.java
+++ src/main/java/cc/towerdefence/minestom/module/chat/ChatModule.java
@@ -20,7 +20,7 @@ import java.util.Optional;
 public class ChatModule extends Module {
     private static final MiniMessage MINI_MESSAGE = MiniMessage.miniMessage();
 
-    protected ChatModule(@NotNull EventNode<Event> eventNode) {
+    public ChatModule(@NotNull EventNode<Event> eventNode) {
         super(eventNode);
     }
 
