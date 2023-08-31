aed70a406e76b91648b7edadb711cc005415e4d0
emortal <me@emortal.dev>
2023-08-31 01:50:32 +0100

    Bump minestom-ce (how did i half bump)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     // Minestom
-    api("dev.hollowcube:minestom-ce:d3bbb8b125")
+    api("dev.hollowcube:minestom-ce:e9d0098418")
     api("net.kyori:adventure-text-minimessage:4.14.0")
     implementation("io.pyroscope:agent:0.11.5")
 
